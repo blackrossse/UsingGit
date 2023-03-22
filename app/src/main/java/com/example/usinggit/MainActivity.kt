dff342261985a07bbe0362b502b922526075f88a
whiterose <nicknickkorol@gmail.com>
2023-03-22 12:20:43 +0300

    Code after pushing

--- app/src/main/java/com/example/usinggit/MainActivity.kt
+++ app/src/main/java/com/example/usinggit/MainActivity.kt
@@ -17,27 +17,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             UsingGitTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                Text("Hello Git")
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    UsingGitTheme {
-        Greeting("Android")
     }
 }
